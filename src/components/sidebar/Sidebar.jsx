dd2b05d9f600560c58ef78b7b7214e680c0ab661
Aamir Hussain <aamirhussainazad786@gmail.com>
2025-12-03 14:30:06 +0530

    first commit

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,57 @@
+import styles from "src/components/sidebar/sidebar.module.css";
+import { CreateGroupModal } from "src/components/createGroupModal/CreateGroupModal";
+import { useSelector, useDispatch } from "react-redux";
+import { changeCurrentActiveGroup } from "src/redux/noteSlice";
+import { useState } from "react";
+
+export const Sidebar = () => {
+  const [openModal, setOpenModal] = useState(false);
+
+  const dispatch = useDispatch();
+
+  const { currentActiveGroup, groups } = useSelector((note) => note.note);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.heading}>
+        <h1>Pocket Notes</h1>
+      </div>
+
+      <div className={styles.group_container}>
+        {groups?.length === 0 && (
+          <p style={{ textAlign: "center" }}>Please create a group.</p>
+        )}
+
+        {groups?.map((group) => (
+          <div
+            key={group.id}
+            className={styles.single_group}
+            style={{
+              background:
+                currentActiveGroup === group.id && "rgb(232, 232, 232)",
+            }}
+            onClick={() => {
+              // setActive(true);
+              dispatch(changeCurrentActiveGroup(group.id));
+            }}
+          >
+            <div
+              className={styles.img_circle}
+              style={{ backgroundColor: group.groupColor }}
+            >
+              <p>{group.groupShortName}</p>
+            </div>
+
+            <h4>{group.groupName}</h4>
+          </div>
+        ))}
+      </div>
+
+      <div onClick={() => setOpenModal(true)} className={styles.create_group}>
+        +
+      </div>
+
+      <CreateGroupModal openModal={openModal} setOpenModal={setOpenModal} />
+    </div>
+  );
+};
